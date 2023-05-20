7ae6b38cec084cc88dd2c1c93e5eca5029fd4101
Yummiii <Leeo05050@protonmail.com>
2023-05-20 03:24:45 -0300

    aa

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["api.zuraaa.com", "127.0.0.1", "media.discordapp.net", "192.168.1.100"]
-  },
-  env: {
-    API_URL: "http://192.168.1.100:5000",
   }
 }
 
